ea838c6db3117b46d7c88187457d7fee01530376
ultimecreation <naserbechata@gmail.com>
2024-09-30 14:29:42 +0200

    update

--- src/main/java/com/jwt_rest_auth/demo/service/AppUserService.java
+++ src/main/java/com/jwt_rest_auth/demo/service/AppUserService.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.jwt_rest_auth.demo.entity.AppUser;
 import com.jwt_rest_auth.demo.repository.AppUserRepository;
 
 @Service
